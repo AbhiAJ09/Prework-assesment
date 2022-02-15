df2718d33d73aeeeb6691d591b1c5fa230a51ee1
Abhimanyu <kumarabhimanyu494@gmail.com>
2022-02-16 00:28:21 +0530

    completing the code for printing star pattern

--- CheckPalindrome.java
+++ CheckPalindrome.java
@@ -1,23 +1,28 @@
 import java.util.*;
 public class CheckPalindrome{
-    public static void main (string [] args)
+    public static void main (String [] args)
 {
 Scanner num=new Scanner(System.in);
-int a=num.nextint();
+System.out.println("Entered a no.");
+int a=num.nextInt();
+int b= a;
 int revNum=0;
 while (a>0)
 {
 revNum = revNum *10 + (a%10);
 a=a/10;
-return rev_num;
+
+
 }
-if (a=revNum)
+if (b==revNum)
 {
     System.out.println("Entered no. is a palindrone no.");
+     System.out.println(revNum);
 }
-else()
+else
 {
     System.out.println("Entered no. is not a palindrone no.") ;
+    System.out.println(revNum);
 }
 
 }
---
+++ Pattern.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Pattern {
+    public static void main (strings [] args) {
+        Scanner star= new Scanner (System.in);
+        System.out.println("Enter the no. of levels of star to be printed");
+        int input=star.nextInt();
+        for (i=0; i<input; i++)
+        {
+            for (j=0; j<input; j++)
+            {
+                System.out.println("*");
+        }
+    }
+}
